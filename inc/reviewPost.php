3fd7c3116fad92b5db49a31f2da4bc94ba23ca69
Pim <79192@rocteraa-student.nl>
2018-02-27 10:27:42 +0100

    made a git

---
+++ inc/reviewPost.php
@@ -0,0 +1,14 @@
+<?php
+$sql ="SELECT * FROM information";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+echo "<table><tr><th>firstname</th><th>lastname</th><th>email</th><th>review</th><th>date</th></tr>";
+// output data of each row
+while ($row = $result->fetch_assoc()) {
+  echo "<tr><td>" . strftime("%d-%I-%Y \n %X", $row["date"]). "</td><td>" . $row["review"]. "</td><td>" . $row["ratingReview"]. "</td></tr>";
+}
+echo "</table>";
+} else {
+echo "0 results";
+} ?>
---
+++ index.php
@@ -0,0 +1,28 @@
+<?php include  'inc/connection.php'?>
+<?php include 'inc/reviewPost.php'?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Guestbook</title>
+  <meta name="description" content="Guestbook">
+  <meta name="keywords" content="HTML,CSS,SQL,PHP">
+  <meta name="author" content="Pim Merkx">
+	<link rel="shortcut icon" href="img/logo.png">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<header id="header"><img src="img/header.png"></header>
+
+<input type="text" class="input" placeholder="First name"></br>
+<input type="text" class="input" placeholder="Last name" required></br>
+<input type="text" class="input" placeholder="E-mail"></br>
+<input type="text" class="input" placeholder="Website adress"></br>
+<textarea class="reviewBox" placeholder="Write your review here..."></textarea></br>
+<button class="button" = type="reset"> Send review </button>
+<button class="button"> Clear </button>
+<td>"<img src=".'uploads/'.$row['image']." />"</td>
+<footer id="footer">
+Guestbook made by PM-Studios © 2018
+</footer>
+</body>
+</html>
