41853a9b36db004faff537acdcf344d71282aca3
Pim <79192@rocteraa-student.nl>
2018-03-07 23:07:15 +0100

    finishing touches

--- Insert.php
+++ Insert.php
@@ -4,11 +4,12 @@ session_start();
 $firstName = $_POST['firstName'];
 $lastName = $_POST['lastName'];
 $Email = $_POST['Email'];
+$messageTitle = $_POST['reviewTitle'];
 $Message = $_POST['message'];
 $websiteAddress = $_POST['websiteAddress'];
 $date  = time();
 $timestamp = strtotime($date);
-$sql5 =  "INSERT INTO information (firstname, lastname, email, review, url, date) VALUES ('$firstName', '$lastName','$Email', '$Message', '$websiteAddress', '$date')";
+$sql5 =  "INSERT INTO information (firstname, lastname, email, reviewTitle, review, url, date) VALUES ('$firstName', '$lastName','$Email', '$messageTitle', '$Message', '$websiteAddress', '$date')";
 
 if ($conn->query($sql5) === true) {
     echo "";
@@ -16,19 +17,6 @@ if ($conn->query($sql5) === true) {
     echo "Error: " . $sql5 . "<br>" . $conn->error;
     die();
 }
-// include_once $_SERVER['DOCUMENT_ROOT'] . '/securimage/securimage.php';
-//
-//     $securimage = new Securimage();
-//
-//     if ($securimage->check($_POST['captcha_code']) == false) {
-//         // the code was incorrect
-//         // you should handle the error so that the form processor doesn't continue
-//
-//         // or you can use the following code if there is no validation or you do not know how
-//         echo "The security code entered was incorrect.<br /><br />";
-//         echo "Please go <a href='javascript:history.go(-1)'>back</a> and try again.";
-//         exit;
-//     }
 
 $alert = "Thanks for your message!";
 echo "<script type='text/javascript'>alert('$alert');</script>";
--- index.php
+++ index.php
@@ -12,17 +12,18 @@
 <body>
 <header id="header"><img src="img/header.png"></header>
   <form action="Insert.php" method="POST">
-    <input type="text" class="input" placeholder="change the color of this page" name="firstName" required></br>
+    <input type="text" class="input" placeholder="First name" name="firstName" required></br>
     <input type="text" class="input" placeholder="Last name" name="lastName" required></br>
     <input type="email" class="input" placeholder="E-mail" name="Email" required></br>
     <input type="text" class="input" placeholder="Website adress" name="websiteAddress" required></br>
+    <input type="text" class="input" placeholder="Review Title" name="reviewTitle" required></br>
     <textarea class="reviewBox" placeholder="Write your review here..." name="message" required></textarea></br>
     <button class="button" type="submit"> Send review </button>
     <button class="button" type="reset"> Clear </button>
 </form>
 </body>
-<?php include 'inc/reviewPost.php'?>
 <footer id="footer">
 Guestbook made by Pim Merkx © 2018
 </footer>
+<?php include 'inc/reviewPost.php'?>
 </html>
